2fd262233b4491c94fd100e2d6ec224b035f0da7
Cadu Valezzi <caduvalezzi@gmail.com>
2026-08-20 00:09:18 -0300

    atualiza número do WhatsApp para o formato correto

--- components/molecules/ContactLinks/constants.ts
+++ components/molecules/ContactLinks/constants.ts
@@ -1,2 +1,2 @@
-export const WHATSAPP_NUMBER = "5511999999999"; // TODO: substituir pelo número real (formato internacional, só dígitos)
+export const WHATSAPP_NUMBER = "5511930342238"; // TODO: substituir pelo número real (formato internacional, só dígitos)
 export const CONTACT_EMAIL = "contato@caduvalezzi.dev"; // TODO: substituir pelo e-mail real
